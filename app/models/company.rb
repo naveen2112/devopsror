af007f81f9e964d3b7e61718d5c08ea5ad8cfa22
kishore-mallow <88766585+kishore-mallow@users.noreply.github.com>
2021-12-16 20:01:48 +0530

    Removed validations in company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,9 +3,4 @@ class Company < ApplicationRecord
   #============================================ Relationships =========================================================
 
   has_many :users, dependent: :destroy
-
-  #============================================ Validations ===========================================================
-
-  validates_presence_of :name
-  validates_uniqueness_of :name
 end
