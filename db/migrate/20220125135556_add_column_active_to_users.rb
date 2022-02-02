0de806182f6813f49805cc073b3458197ef9187a
kishore-mallow <88766585+kishore-mallow@users.noreply.github.com>
2022-02-02 11:23:23 +0530

    Fixed issues in migration

--- db/migrate/20220125135556_add_column_active_to_users.rb
+++ db/migrate/20220125135556_add_column_active_to_users.rb
@@ -1,6 +1,6 @@
 class AddColumnActiveToUsers < ActiveRecord::Migration[6.1]
   def change
-    safety_assured { remove_column :users, :terms_and_condition }
+    # safety_assured { remove_column :users, :terms_and_condition }
 
     add_column :users, :invite, :boolean, default: false
     add_column :users, :active, :boolean, default: false
