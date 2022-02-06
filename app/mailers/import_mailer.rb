b77697c879cd896933678d9e641e4c51ae24f2d8
kishore-mallow <88766585+kishore-mallow@users.noreply.github.com>
2022-02-06 20:21:12 +0530

    Fixed issues in csv file upload

--- app/mailers/import_mailer.rb
+++ app/mailers/import_mailer.rb
@@ -1,10 +1,11 @@
 class ImportMailer < ApplicationMailer
 
-  def import_notification(import, email, total_records_count=nil, error_records_count=nil)
+  def import_notification(import, email, total_records_count=nil, error_records_count=nil, headers=nil)
     @status = import.status
     @email = email
     @total_records_count = total_records_count
     @error_records_count = error_records_count
+    @headers = headers
 
     attachments['error_list.csv'] = URI.open(import.error_file.url).read if import.failed?
 
