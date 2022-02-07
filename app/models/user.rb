def42dffd5fd7750157bae2f54c16d14e6e81e62
kishore-mallow <88766585+kishore-mallow@users.noreply.github.com>
2022-02-07 11:41:49 +0530

    removed validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
 
   #========================================= Relationships ============================================================
 
@@ -23,6 +23,7 @@ class User < ApplicationRecord
   #========================================= Validations ==============================================================
 
   validates_presence_of :first_name
+  validates_uniqueness_of :email, scope: :company_id
 
   #======================================== Enum ======================================================================
 
