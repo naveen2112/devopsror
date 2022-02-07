c736ccf94534dd29fe83bd317bfb49ea7d26b33c
kishore-mallow <88766585+kishore-mallow@users.noreply.github.com>
2022-02-07 14:47:17 +0530

    fixed email validation issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,8 +22,8 @@ class UsersController < ApplicationController
   def validate_email_without_current_user
     return render plain: false unless params[:user][:email].present?
 
-    user_selected = params[:user_id].present? ? current_company.users.find(params[:user_id]) : current_user
-    user = current_company.users.where.not(id: user_selected.id).where("LOWER(email) = ?", params[:user][:email].downcase)
+    user_selected = params[:user_id].present? ? User.find(params[:user_id]) : current_user
+    user = User.where.not(id: user_selected.id).where("LOWER(email) = ?", params[:user][:email].downcase)
     render plain: user.empty? ? 'true' : 'false'
   end
 
