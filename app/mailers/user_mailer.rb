79fab84271cb013c60a0998750dae8ce7457c694
kishore-mallow <88766585+kishore-mallow@users.noreply.github.com>
2022-02-07 13:54:13 +0530

    Email subject changes

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,6 @@ class UserMailer < ApplicationMailer
     company = Company.find(company_id)
     @user = company.users.find(user_id)
 
-    mail to: @user.email, subject: "Invite email"
+    mail to: @user.email, subject: "Join #{company.name} on SoVocal"
   end
 end
