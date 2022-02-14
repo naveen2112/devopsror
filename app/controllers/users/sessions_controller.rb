c9d1e13c02af5e8dc5835faf8cd80ee5f0552074
kishore-mallow <88766585+kishore-mallow@users.noreply.github.com>
2022-02-14 18:57:44 +0530

    removed extra line in session controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,6 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
-
-
+  
   def create
     self.resource = warden.authenticate!(auth_options)
     set_flash_message!(:notice, :signed_in)
