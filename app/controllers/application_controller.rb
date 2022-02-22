4003c6732de32309f58f5a55224866097fd3d068
kishore-mallow <88766585+kishore-mallow@users.noreply.github.com>
2022-02-22 15:27:45 +0530

    Added fix for csrf attack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery prepend: true, with: :exception
+  protect_from_forgery with: :null_session
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :current_company
