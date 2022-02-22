46214cc3ded633dc2343f53160f1f4a57f4f3e72
kishore-mallow <88766585+kishore-mallow@users.noreply.github.com>
2022-02-22 16:16:26 +0530

    Fixed protect from forgery issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
+  protect_from_forgery only: :admin_action?
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :current_company
